981c2b967973f39d6ce039e0463bf419b450dd01
U-ARINC\nsarker <nsarker@NSARKER529708.arinc.com>
2016-08-01 08:25:39 -0400

    added docs, 405 error handling, and py2 compatibility

--- base_app.py
+++ base_app.py
@@ -1,7 +1,7 @@
 import json
 
 from klein import Klein
-from werkzeug.exceptions import NotFound
+from werkzeug.exceptions import MethodNotAllowed, NotFound
 
 
 def getApp():
@@ -14,7 +14,21 @@ def getApp():
 
     @app.handle_errors(NotFound)
     def routeNotFound(request, failure):
+        """
+        Endpoint not found - 404
+        """
         request.setResponseCode(404)
-        return json.dumps({'error': 'Invalid endpoint %s' % (request.path)})
+        invalid_path = request.path.decode('utf-8')
+        return json.dumps({'error': 'Endpoint (%s) not found' % (invalid_path)})
+
+    @app.handle_errors(MethodNotAllowed)
+    def invalidMethod(request, failure):
+        """
+        Invalid method - 405
+        """
+        request.setResponseCode(405)
+        method = request.method.decode('utf-8')
+        endpoint = request.path.decode('utf-8')
+        return json.dumps({'error': 'Invalid method call (%s) at %s endpoint' % (method, endpoint)})
 
     return app
